5bbc2be84aa014f3a1a0b1b3df3510a09afae75f
Zconj095 <96359501+Zconj095@users.noreply.github.com>
2023-09-06 22:31:22 -0400

    Add files via upload

---
+++ EXTENSIONONEPERSONGRAMMAR.py
@@ -0,0 +1,6 @@
+from ExampleScriptAAA import *
+from ExampleScriptBBB import *
+from ExampleScriptCCC import *
+from ExampleScriptDDD import *
+ZTHE,ZWORD,ZPHILOSOPHER,ZIS,ZAN,ZPERSON
+ZTHE,ZWORD,ZSUMMONER,ZIS,ZAN,ZPERSON
